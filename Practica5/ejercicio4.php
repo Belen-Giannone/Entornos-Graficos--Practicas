15064fac79ee57854c20a7da0deda38139562862
Marisol <marisolavila807@gmail.com>
2026-09-15 23:14:23 -0300

    Agrego ejercicio 4

---
+++ Practica5/ejercicio4.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+
+if (isset($_SESSION["contador"])) {
+    $_SESSION["contador"]++;
+} else {
+    $_SESSION["contador"] = 1;
+}
+
+echo "Has visitado esta página " . $_SESSION["contador"] . " veces durante tu sesión.";
+?>
